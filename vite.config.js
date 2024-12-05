31fb13149a8fc15daab285a64259300cf11b8967
benjaminskarding <benjamin.skarding@hotmail.com>
2024-12-05 10:08:14 +0100

    changed build output folder from docs to dist

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  appType: "mpa",
-  base: "/",
+  appType: 'mpa',
+  base: '/',
   build: {
-    target: "esnext",
-    outDir: "docs",
+    target: 'esnext',
+    outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "./index.html"),
-        login: resolve(__dirname, "./auth/login/index.html"),
-        auth: resolve(__dirname, "./auth/index.html"),
-        register: resolve(__dirname, "./auth/register/index.html"),
-        profile: resolve(__dirname, "./profile/index.html"),
-        post: resolve(__dirname, "./listing/index.html"),
-        createPost: resolve(__dirname, "./listing/create/index.html"),
+        main: resolve(__dirname, './index.html'),
+        login: resolve(__dirname, './auth/login/index.html'),
+        auth: resolve(__dirname, './auth/index.html'),
+        register: resolve(__dirname, './auth/register/index.html'),
+        profile: resolve(__dirname, './profile/index.html'),
+        post: resolve(__dirname, './listing/index.html'),
+        createPost: resolve(__dirname, './listing/create/index.html'),
       },
     },
   },
